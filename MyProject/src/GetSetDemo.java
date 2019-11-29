099eedc3d5233322c30c145befbe7066565c1792
Ramesh <Ramesh@RAMESH>
2019-11-29 11:12:00 +0530

    Test Page ....

---
+++ MyProject/src/GetSetDemo.java
@@ -0,0 +1,25 @@
+
+public class GetSetDemo {
+int MyMobileNumber;
+String MyExamName;
+int MyHtno;
+public int getMyMobileNumber() {
+	return MyMobileNumber;
+}
+public void setMyMobileNumber(int myMobileNumber) {
+	MyMobileNumber = myMobileNumber;
+}
+public String getMyExamName() {
+	return MyExamName;
+}
+public void setMyExamName(String myExamName) {
+	MyExamName = myExamName;
+}
+public int getMyHtno() {
+	return MyHtno;
+}
+public void setMyHtno(int myHtno) {
+	MyHtno = myHtno;
+}
+
+}
---
+++ MyProject/src/GetterDemo.java
@@ -0,0 +1,13 @@
+
+public class GetterDemo {
+int MyAccountNumber;
+
+public int getMyAccountNumber() {
+	return MyAccountNumber;
+}
+
+public void setMyAccountNumber(int myAccountNumber) {
+	MyAccountNumber = myAccountNumber;
+}
+	
+}
